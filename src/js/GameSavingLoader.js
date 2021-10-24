102ad01ef63927887f2bc444932207b26cc77ad0
Voinov Nikita <nikita-nikita2012@mail.ru>
2021-10-24 22:29:16 +0300

    first commit

---
+++ src/js/GameSavingLoader.js
@@ -0,0 +1,12 @@
+/* eslint-disable linebreak-style */
+import json from './parser';
+import read from './reader';
+import GameSaving from './GameSaving';
+
+export default class GameSavingLoader {
+  static async load() {
+    const reader = await read();
+    const parser = await json(reader);
+    return new GameSaving(JSON.parse(parser));
+  }
+}
---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-console */
+import GameSavingLoader from './GameSavingLoader';
+
+(async () => {
+  try {
+    const saving = await GameSavingLoader.load();
+    console.log(saving);
+  } catch (error) {
+    console.log(error);
+  }
+})();
